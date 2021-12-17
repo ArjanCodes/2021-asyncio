af0138eedd6396e04c19a9161445b3911bd352ae
Arjan <arjan@quarterfall.com>
2021-12-17 17:07:13 +0100

    Added example code.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 2021-asyncio
+# Asynchronous operations
+
+This repository contains the example code of the video on Asynchronous Operations using Asyncio. Here's the link to the video: https://youtu.be/2IW-ZEui4h4.
---
+++ after-1/main.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
+from iot.message import Message, MessageType
+from iot.service import IOTService
+
+
+async def main() -> None:
+    # create a IOT service
+    service = IOTService()
+
+    # create and register a few devices
+    hue_light = HueLightDevice()
+    speaker = SmartSpeakerDevice()
+    toilet = SmartToiletDevice()
+    hue_light_id = await service.register_device(hue_light)
+    speaker_id = await service.register_device(speaker)
+    toilet_id = await service.register_device(toilet)
+
+    # create a few programs
+    wake_up_program = [
+        Message(hue_light_id, MessageType.SWITCH_ON),
+        Message(speaker_id, MessageType.SWITCH_ON),
+        Message(speaker_id, MessageType.PLAY_SONG, "Miles Davis - Kind of Blue"),
+    ]
+
+    sleep_program = [
+        Message(hue_light_id, MessageType.SWITCH_OFF),
+        Message(speaker_id, MessageType.SWITCH_OFF),
+        Message(toilet_id, MessageType.FLUSH),
+        Message(toilet_id, MessageType.CLEAN),
+    ]
+
+    # run the programs
+    await service.run_program(wake_up_program)
+    await service.run_program(sleep_program)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
